4e68810a2e5103aaf52984cc68ef058fef36f9fe
Andrew Rosiclair <arosiclair@gmail.com>
2020-09-27 23:01:33 -0400

    moved App component to UI

--- ui/src/App.vue
+++ ui/src/App.vue
@@ -1,28 +1,91 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <Header v-if="loggedIn" @logged-off="onLoggedOff"></Header>
+    <div class="container content">
+      <List v-if="loggedIn"></List>
+      <Login v-else @logged-in="onLoggedIn"></Login>
+    </div>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Header from './components/Header'
+import Login from './components/Login'
+import List from './components/List'
+import currentUser from './modules/user'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    Header,
+    Login,
+    List
+  },
+
+  data () {
+    return {
+      loggedIn: false
+    }
+  },
+
+  mounted: async function () {
+    console.log('Beginning login test...')
+    var result = await currentUser.updateUserData()
+    this.loggedIn = result.success
+  },
+
+  methods: {
+    onLoggedIn: function () {
+      this.loggedIn = true
+    },
+    onLoggedOff: function () {
+      this.loggedIn = false
+    }
   }
 }
 </script>
 
 <style>
+@import url('https://fonts.googleapis.com/css?family=Rubik&display=swap');
+
+body {
+  background-color: #fafafa;
+  color: #212121;
+  font-family: 'Rubik', sans-serif;
+}
+
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
+}
+
+.content {
   text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+  max-width: 750px;
+  padding-top: 100px;
+  padding-bottom: 100px;
+}
+
+.centered-container {
+    display: flex;
+    align-items: center;
+}
+
+.centered-container.split {
+    justify-content: space-between;
+}
+
+.material-icons {
+    color: #c7c7c7;
+    font-size: 30px;
+    padding: 10px;
+    border-radius: 50%;
+    cursor: pointer;
+}
+.material-icons.actionable:hover {
+    color: #28b5f4;
+    background-color: #0000000f;
+    cursor: pointer;
 }
 </style>

05c7c08f4b990d8b3ad8a55360d290a5c98e3f17
Andrew Rosiclair <arosiclair@gmail.com>
2020-12-07 23:26:34 -0500

    point webpack dev server https creds to nginx creds

--- ui/vue.config.js
+++ ui/vue.config.js
@@ -1,6 +1,10 @@
+const fs = require('fs')
+
 module.exports = {
   devServer: {
-    https: true
+    https: true,
+    key: fs.readFileSync('../nginx/conf/nginx.key'),
+    cert: fs.readFileSync('../nginx/conf/nginx.cert')
   },
   configureWebpack: {
     devtool: 'source-map'

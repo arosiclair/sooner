d503c48d2ab1a616ecbc2264c8520b75d9186379
Andrew Rosiclair <arosiclair@gmail.com>
2020-12-11 22:00:18 -0500

    added db collection and TTL index for resetRequests

--- mongodb/init.js
+++ mongodb/init.js
@@ -10,3 +10,6 @@ db.users.createIndex({ 'email': 1 }, { 'unique': true })
 
 db.createCollection('sessions')
 db.sessions.createIndex({ 'createdAt': 1 }, { 'expireAfterSeconds': 3600 })
+
+db.createCollection('resetRequests')
+db.resetRequests.createIndex({ 'createdAt': 1 }, { 'expireAfterSeconds': 3600 })

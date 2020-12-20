d7d018f8a4af0b9d3dbcd87f9080d00f58a34650
Andrew Rosiclair <arosiclair@gmail.com>
2020-12-20 00:26:12 -0500

    ignore vue config options for dev in prod

--- ui/vue.config.js
+++ ui/vue.config.js
@@ -1,12 +1,18 @@
 const fs = require('fs')
 
-module.exports = {
-  devServer: {
-    https: true,
-    key: fs.readFileSync('../nginx/conf/nginx.key'),
-    cert: fs.readFileSync('../nginx/conf/nginx.cert')
-  },
-  configureWebpack: {
-    devtool: 'source-map'
+let devOptions = {}
+if (process.env.NODE_ENV === 'development') {
+  devOptions = {
+    devServer: {
+      https: true,
+      key: fs.readFileSync('../nginx/conf/nginx.key'),
+      cert: fs.readFileSync('../nginx/conf/nginx.cert')
+    },
+    configureWebpack: {
+      devtool: 'source-map'
+    }
   }
+}
+module.exports = {
+  ...devOptions
 }

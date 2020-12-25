e488e1f0abc2ce00581781cfd03e19469846bb91
Andrew Rosiclair <arosiclair@gmail.com>
2020-12-25 15:26:21 -0500

    removed unnecessary tags

--- ui/src/components/PasswordReqs.vue
+++ ui/src/components/PasswordReqs.vue
@@ -2,20 +2,10 @@
   <b-alert show variant="info" class="text-left">
     <h5 class="text-center">Password Requirements</h5>
     <ul class="m-0 pl-4">
-      <li>Between 8 and 30 characters</li>
+      <li>Between 8 and 32 characters</li>
       <li>At least one lowercase letter</li>
       <li>At least one uppercase letter</li>
       <li>At least one number</li>
     </ul>
   </b-alert>
 </template>
-
-<script>
-export default {
-
-}
-</script>
-
-<style>
-
-</style>

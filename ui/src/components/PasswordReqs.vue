a37b85d9ebfc8b580c243bfa378920e98ec41f3e
Andrew Rosiclair <arosiclair@gmail.com>
2020-12-24 20:46:55 -0500

    added initial password reset page

---
+++ ui/src/components/PasswordReqs.vue
@@ -0,0 +1,21 @@
+<template>
+  <b-alert show variant="info" class="text-left">
+    <h5 class="text-center">Password Requirements</h5>
+    <ul class="m-0 pl-4">
+      <li>Between 8 and 30 characters</li>
+      <li>At least one lowercase letter</li>
+      <li>At least one uppercase letter</li>
+      <li>At least one number</li>
+    </ul>
+  </b-alert>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
---
+++ ui/src/components/PasswordReset.vue
@@ -0,0 +1,73 @@
+<template>
+  <div>
+    <div class="centered-container justify-content-center m-4">
+      <img id="logo" :src="Logo" class="mr-3"/>
+      <span class="text-lg">Read It Now</span>
+    </div>
+    <h3 class="mb-4">Password Reset</h3>
+    <div class="d-flex justify-content-center">
+      <PasswordRequirements/>
+    </div>
+    <div class="shadow-sm rounded overflow-hidden mb-3">
+      <input
+        v-model="password"
+        type="password"
+        placeholder="New password"
+        @keyup.enter="submit"
+        class="lg"
+        :class="{ error: error && !validPass }" />
+      <input
+        v-model="passwordConfirm"
+        type="password"
+        placeholder="Confirm password"
+        @keyup.enter="submit"
+        class="lg"
+        :class="{ error: error && !validPass }" />
+    </div>
+    <button class="btn-lg text-lg shadow-sm rounded p-3" type="button" @click="submit" :disabled="loading">
+        <b-spinner v-if="loading"/>
+        <span v-else>
+          SUBMIT
+        </span>
+      </button>
+  </div>
+</template>
+
+<script>
+import Logo from '../assets/logo-rounded.png'
+import PasswordRequirements from './PasswordReqs'
+
+export default {
+  components: {
+    PasswordRequirements
+  },
+
+  data () {
+    return {
+      Logo,
+      password: '',
+      passwordConfirm: '',
+      error: false,
+      loading: false
+    }
+  },
+
+  computed: {
+    validPass () {
+      return true
+    }
+  },
+
+  methods: {
+    submit () {
+      return true
+    }
+  }
+}
+</script>
+
+<style scoped>
+#logo {
+  width: 50px;
+}
+</style>

8467596899a168b834df3929b7fad43139066e26
Andrew Rosiclair <arosiclair@gmail.com>
2020-12-27 18:14:04 -0500

    added big submit btn with common styling

---
+++ ui/src/components/BigSubmitBtn.vue
@@ -0,0 +1,18 @@
+<template>
+  <button class="btn-lg text-lg shadow-sm rounded p-3" type="button" @click="onSubmit" :disabled="loading">
+    <b-spinner v-if="loading"/>
+    <span v-else>
+      {{ label }}
+    </span>
+  </button>
+</template>
+
+<script>
+export default {
+  props: [
+    'onSubmit',
+    'loading',
+    'label'
+  ]
+}
+</script>

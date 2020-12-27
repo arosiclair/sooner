bc7c69f15014badce729633b046f1f8eb09720c9
Andrew Rosiclair <arosiclair@gmail.com>
2020-12-27 00:22:52 -0500

    vue routing with constants

--- ui/src/router/index.js
+++ ui/src/router/index.js
@@ -2,29 +2,38 @@ import Vue from 'vue'
 import VueRouter from 'vue-router'
 import List from '../components/List'
 
+export const RouteNames = {
+  List: 'List',
+  Login: 'Login',
+  PasswordReset: 'PasswordReset',
+  PasswordResetSuccess: 'PasswordResetSuccess'
+}
+
 Vue.use(VueRouter)
 
 const routes = [
   {
     path: '/list',
-    name: 'List',
+    name: RouteNames.List,
     component: List
   },
   {
     path: '/login',
-    name: 'Login',
+    name: RouteNames.Login,
     // route level code-splitting
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../components/Login.vue')
+    component: () => import(/* webpackChunkName: "login" */ '../components/Login.vue')
   },
   {
     path: '/passwordReset',
-    name: 'PasswordReset',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../components/PasswordReset.vue')
+    name: RouteNames.PasswordReset,
+    component: () => import(/* webpackChunkName: "passwordreset" */ '../components/PasswordReset/PasswordReset')
+  },
+  {
+    path: '/passwordReset/success',
+    name: RouteNames.PasswordResetSuccess,
+    component: () => import(/* webpackChunkName: "passwordresetsuccess" */ '../components/PasswordReset/Success')
   }
 ]
 

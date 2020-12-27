81f02374fc683cb6d45929e560155974ff0cb1f4
Andrew Rosiclair <arosiclair@gmail.com>
2020-12-27 18:14:15 -0500

    added common letter head

---
+++ ui/src/components/LetterHead.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="centered-container justify-content-center m-4">
+    <img id="logo" :src="Logo" class="mr-3"/>
+    <span class="text-lg">Read It Now</span>
+  </div>
+</template>
+
+<script>
+import Logo from '@/assets/logo-rounded.png'
+export default {
+  data () {
+    return {
+      Logo
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>

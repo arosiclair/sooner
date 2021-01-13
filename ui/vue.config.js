aab4db7af21abb84d5d658f38f772cd35c163b5f
Andrew Rosiclair <arosiclair@gmail.com>
2021-01-12 22:44:53 -0500

    only use service worker in prod

--- ui/vue.config.js
+++ ui/vue.config.js
@@ -1,40 +1,39 @@
-const path = require('path')
 const { InjectManifest } = require('workbox-webpack-plugin')
 const CopyPlugin = require('copy-webpack-plugin')
 
-const config = {
-  configureWebpack: {
-    resolve: {
-      alias: {
-        '~': path.resolve(__dirname, 'src/')
-      }
-    },
-    plugins: [
-      new CopyPlugin({
-        patterns: [
-          { from: 'public/', to: './' }
-        ]
-      }),
-      new InjectManifest({
-        swSrc: './src/service-worker.js'
-      })
-    ]
-  }
-}
+let config
 
-if (process.env.NODE_ENV === 'development') {
-  config.devServer = {
-    proxy: {
-      '^/api': {
-        target: 'http://localhost:3000',
-        pathRewrite: { '^/api': '' },
-        ws: true,
-        changeOrigin: true
-      }
+if (process.env.NODE_ENV === 'production') {
+  config = {
+    configureWebpack: {
+      plugins: [
+        new CopyPlugin({
+          patterns: [
+            { from: 'public/', to: './' }
+          ]
+        }),
+        new InjectManifest({
+          swSrc: './src/service-worker.js'
+        })
+      ]
     }
   }
-
-  config.configureWebpack.devtool = 'source-map'
+} else {
+  config = {
+    devServer: {
+      proxy: {
+        '^/api': {
+          target: 'http://localhost:3000',
+          pathRewrite: { '^/api': '' },
+          ws: true,
+          changeOrigin: true
+        }
+      }
+    },
+    configureWebpack: {
+      devtool: 'source-map'
+    }
+  }
 }
 
 module.exports = config

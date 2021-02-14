9c8d5b12ba0d1880ab0d0f6bc3cc0e54a1164355
Andrew Rosiclair <arosiclair@gmail.com>
2021-02-13 23:22:45 -0500

    added context menu action for links

--- browser-ext/src/background.js
+++ browser-ext/src/background.js
@@ -1,4 +1,20 @@
-// eslint-disable-next-line no-unused-vars
-browser.runtime.onMessage.addListener(function (request, sender, sendResponse) {
-  console.log('Hello from the background')
+import { addLink } from './api'
+
+browser.contextMenus.create({
+  id: 'add-link-to-sooner',
+  title: 'Add to Sooner',
+  contexts: ['link']
+})
+
+browser.contextMenus.onClicked.addListener(async (info, tab) => {
+  if (info.menuItemId === 'add-link-to-sooner') {
+    const linkUrl = info.linkUrl
+    console.log('[sooner-ext] context menu link: ' + linkUrl)
+
+    try {
+      addLink(linkUrl)
+    } catch (error) {
+      console.error('[sooner-ext] failed to add a link')
+    }
+  }
 })

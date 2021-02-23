5ddc3f7441718dacce47622bf349e6967ea401e3
Andrew Rosiclair <arosiclair@gmail.com>
2021-02-22 22:56:48 -0500

    added dupe prevention for links

--- server/src/daos/lists.js
+++ server/src/daos/lists.js
@@ -4,7 +4,11 @@ const { getUserById } = require('./users')
 async function getListById (listId) {
   if (!listId) return null
 
-  return geoffrey.getLists().findOne({ _id: listId })
+  try {
+    return await geoffrey.getLists().findOne({ _id: listId })
+  } catch (error) {
+    return null
+  }
 }
 
 async function updateLinks (listId, links) {
@@ -49,6 +53,14 @@ async function createListForUser (userId) {
 async function addLink (userId, name, siteName, link, addedOn = new Date()) {
   if (!userId || !name || !siteName || !link) return false
 
+  const listId = await getListIdForUser(userId)
+
+  // check for a dupe and return it instead
+  const existingLink = await checkExists(listId, link)
+  if (existingLink) {
+    return existingLink._id
+  }
+
   const newLink = {
     _id: geoffrey.getObjectId(),
     name,
@@ -57,7 +69,6 @@ async function addLink (userId, name, siteName, link, addedOn = new Date()) {
     addedOn: addedOn || new Date()
   }
 
-  const listId = await getListIdForUser(userId)
   try {
     await geoffrey.getLists().updateOne({ _id: listId }, { $push: { links: newLink } })
   } catch (error) {
@@ -75,6 +86,15 @@ async function deleteLink (userId, linkId) {
   const linkObjId = geoffrey.getObjectId(linkId)
   geoffrey.getLists().updateOne({ _id: listId }, { $pull: { links: { _id: linkObjId } } })
   return true
+}
+
+async function checkExists (listId, link) {
+  if (!listId || !link) return false
+
+  const list = await getListById(listId)
+  if (!list) return false
+
+  return list.links.find(linkItem => linkItem.link === link)
 }
 
 module.exports = {

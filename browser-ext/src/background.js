221d8843192d0f364bc6bffc0704b0f014b9b4d1
Andrew Rosiclair <arosiclair@gmail.com>
2021-02-24 23:28:16 -0500

    added extension menu option to open web app

--- browser-ext/src/background.js
+++ browser-ext/src/background.js
@@ -7,6 +7,12 @@ browser.contextMenus.create({
 })
 
 browser.contextMenus.create({
+  id: 'go-to-sooner',
+  title: 'Go to Sooner',
+  contexts: ['browser_action']
+})
+
+browser.contextMenus.create({
   id: 'logout',
   title: 'Log out',
   contexts: ['browser_action']
@@ -21,6 +27,9 @@ browser.contextMenus.onClicked.addListener(async (info, tab) => {
       } catch (error) {
         console.error('[sooner-ext] failed to add a link')
       }
+      break
+    case 'go-to-sooner':
+      window.open('https://www.sooner.app/list')
       break
     case 'logout':
       try {

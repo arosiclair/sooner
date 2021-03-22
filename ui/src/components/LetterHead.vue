79ea9e39806ae55ad9ad345c10ade63aa910012c
Andrew Rosiclair <arosiclair@gmail.com>
2021-03-21 21:38:43 -0400

    updated LetterHead to match landing page

--- ui/src/components/LetterHead.vue
+++ ui/src/components/LetterHead.vue
@@ -1,23 +1,53 @@
 <template>
-  <div class="centered-container justify-content-center p-4">
-    <img id="logo" :src="Logo" class="mr-3"/>
-    <span class="text-lg">Sooner</span>
-  </div>
+  <router-link
+    class="home-link"
+    :to="homeRoute"
+  >
+    <div class="letter-head-container p-3">
+      <img
+        id="logo"
+        :src="Logo"
+        class="mr-4"
+      >
+      <h1 class="m-0">
+        Sooner
+      </h1>
+    </div>
+  </router-link>
 </template>
 
 <script>
 import Logo from '@/assets/logo-rounded.png'
+import { RouteNames } from '../router'
 export default {
   data () {
     return {
-      Logo
+      Logo,
+      homeRoute: {
+        name: RouteNames.Landing
+      }
     }
   }
 }
 </script>
 
 <style scoped>
+.home-link {
+ color: inherit;
+ text-decoration: none;
+}
+.letter-head-container {
+ display: flex;
+ justify-content: center;
+ align-items: center;
+}
+
 #logo {
   width: 50px;
 }
+
+h1 {
+  font-weight: 900;
+}
+
 </style>

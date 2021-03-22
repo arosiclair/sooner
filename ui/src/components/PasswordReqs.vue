dedd6be3bac03c1cdcccdfcb14876585f0705b48
Andrew Rosiclair <arosiclair@gmail.com>
2021-03-21 21:17:36 -0400

    PasswordReqs formatting

--- ui/src/components/PasswordReqs.vue
+++ ui/src/components/PasswordReqs.vue
@@ -1,6 +1,12 @@
 <template>
-  <b-alert show variant="info" class="text-left">
-    <h5 class="text-center">Password Requirements</h5>
+  <b-alert
+    show
+    variant="info"
+    class="text-left"
+  >
+    <h5 class="text-center">
+      Password Requirements
+    </h5>
     <ul class="m-0 pl-4">
       <li>Between 8 and 32 characters</li>
       <li>At least one lowercase letter</li>

210d2ceb35abf684a0eb65b3b0c383c2807e47e1
Andrew Rosiclair <arosiclair@gmail.com>
2021-05-16 20:07:57 -0400

    using common auth middleware

--- packages/notifications/src/app.js
+++ packages/notifications/src/app.js
@@ -1,6 +1,6 @@
 const express = require('express')
-const cookieParser = require('cookie-parser')
 const logger = require('morgan')
+const cookieSession = require('cookie-session')
 
 const indexRouter = require('./routes/index')
 
@@ -9,7 +9,17 @@ const app = express()
 app.use(logger('dev'))
 app.use(express.json())
 app.use(express.urlencoded({ extended: false }))
-app.use(cookieParser())
+
+// Cookies
+if (!process.env.SECRET_SESSION_KEY) {
+  throw new Error('SECRET_SESSION_KEY is not defined')
+}
+
+app.use(cookieSession({
+  name: 'soonerSession',
+  secret: process.env.SECRET_SESSION_KEY,
+  secure: process.env.NODE_ENV !== 'development'
+}))
 
 app.use('/', indexRouter)
 
--- packages/notifications/src/routes/index.js
+++ packages/notifications/src/routes/index.js
@@ -1,7 +1,8 @@
 const express = require('express')
 const subscription = require('./subscription')
 const router = express.Router()
+const auth = require('@sooner/middleware/auth')
 
-router.use('/subscription', subscription)
+router.use('/subscription', auth, subscription)
 
 module.exports = router

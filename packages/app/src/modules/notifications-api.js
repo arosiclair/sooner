832074fce5f13877ca9a5a6bb2d6e6c0f39c2b46
Andrew Rosiclair <arosiclair@gmail.com>
2021-08-21 15:20:52 -0400

    updated push api env var naming

--- packages/app/src/modules/notifications-api.js
+++ packages/app/src/modules/notifications-api.js
@@ -2,5 +2,5 @@ import axios from 'axios'
 
 export default axios.create({
   withCredentials: true,
-  baseURL: process.env.VUE_APP_NOTIFICATIONS_API_URL
+  baseURL: process.env.VUE_APP_PUSH_API_URL
 })

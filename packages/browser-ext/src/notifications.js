f00f5c3f37b50b40093ccb8d25681489fb391a27
Andrew Rosiclair <arosiclair@gmail.com>
2021-08-30 22:05:56 -0400

    added confirmation notifications for adding links via context menu

---
+++ packages/browser-ext/src/notifications.js
@@ -0,0 +1,21 @@
+const basicNotification = {
+  type: 'basic',
+  iconUrl: 'icons/96.png',
+  isClickable: true
+}
+
+module.exports.showPageAddedNotification = () => {
+  browser.notifications.create({
+    title: 'Page added',
+    message: 'Page added to your Sooner list',
+    ...basicNotification
+  })
+}
+
+module.exports.showPageAddFailedNotification = () => {
+  browser.notifications.create({
+    title: "Couldn't add that page",
+    message: 'Are you still logged in?',
+    ...basicNotification
+  })
+}

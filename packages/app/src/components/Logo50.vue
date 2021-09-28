6ad669a84eb5a55747b0d310674e8446e2505404
arosiclair <arosiclair@gmail.com>
2021-09-28 19:18:41 -0400

    fix path for logo on /resetRequest/success page

--- packages/app/src/components/Logo50.vue
+++ packages/app/src/components/Logo50.vue
@@ -1,7 +1,7 @@
 <template>
   <img
-    srcset="img/logo-rounded-50.png 1x, img/logo-rounded-100.png 2x, img/logo-rounded-150.png 3x"
-    src="img/logo-rounded-150"
+    srcset="/img/logo-rounded-50.png 1x, /img/logo-rounded-100.png 2x, /img/logo-rounded-150.png 3x"
+    src="/img/logo-rounded-150"
     alt="Sooner's logo"
     class="logo-50"
   >

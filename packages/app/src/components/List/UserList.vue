03366267e600a507dd98c9c97bf4a5e6444c9d47
arosiclair <arosiclair@gmail.com>
2022-02-18 23:38:05 -0500

    simplified list refresh

--- packages/app/src/components/List/UserList.vue
+++ packages/app/src/components/List/UserList.vue
@@ -124,17 +124,9 @@ export default {
         }
 
         this.links = result.data.list || []
-        if (this.links.length === 0) {
-          this.empty = true
-        } else {
-          this.empty = false
-        }
+        this.empty = !this.links.length
 
-        const numExpired = result.data.numExpired
-        if (numExpired) {
-          const links = numExpired > 1 ? 'links' : 'link'
-          this.$toast.info(`${numExpired} ${links} expired since you last visited`, { timeout: false })
-        }
+        this.expiredPrompt(result.data.numExpired)
 
         done()
       })
@@ -158,6 +150,12 @@ export default {
 
         done()
       })
+    },
+    expiredPrompt (numExpired) {
+      if (numExpired) {
+        const links = numExpired > 1 ? 'links' : 'link'
+        this.$toast.info(`${numExpired} ${links} expired since you last visited`, { timeout: false })
+      }
     },
     sharePrompt () {
       if (this.$route.query.share) {

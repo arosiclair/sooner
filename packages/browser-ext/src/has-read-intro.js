f8c1430c9e5666384336d0806a1b67f0cf03a44a
Andrew Rosiclair <arosiclair@expensify.com>
2023-11-25 17:06:20 -0500

    stop logging browser.storage

--- packages/browser-ext/src/has-read-intro.js
+++ packages/browser-ext/src/has-read-intro.js
@@ -1,7 +1,6 @@
 const KEY = 'HAS_READ_INTRO'
 
 export const hasReadIntro = async () => {
-  console.log(browser.storage)
   try {
     const results = await browser.storage.local.get(KEY)
     return results[KEY]
